964e7b5fc03444bcb30fb87b65866fe9e9fecab8
Kamich-GP <yurochkagameplus@gmail.com>
2025-03-12 21:16:13 +0500

    urls, views

--- index/views.py
+++ index/views.py
@@ -15,3 +15,30 @@ def home_page(request):
         'products': products
     }
     return render(request, 'home.html', context)
+
+
+# Вывод товаров по категории
+def category_page(request, pk):
+    # Достаем данные из БД
+    category = Category.objects.get(id=pk)
+    products = Product.objects.filter(product_category=category)
+
+    # Передаем данные на фронт
+    context = {
+        'category': category,
+        'products': products
+    }
+
+    return render(request, 'category.html', context)
+
+# Вывод определенного товара
+def product_page(request, pk):
+    # Достаем данные из БД
+    product = Product.objects.get(id=pk)
+
+    # Отправляем данные на фронт
+    context = {
+        'product': product
+    }
+
+    return render(request, 'product.html', context)
